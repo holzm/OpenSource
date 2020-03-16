75f5cc549d93c76256e7799fe6908aecd1153f3e
holzm <54302524+holzm@users.noreply.github.com>
2020-03-16 22:34:41 +0800

    opencv_extended.cpp注释

--- Head/opencv_extended.cpp
+++ Head/opencv_extended.cpp
@@ -17,21 +17,20 @@ namespace cvex
 	using cv::waitKey;
 
 	using std::vector;
-
 	void showHist(const Mat img)
 	{
 		Mat hist;
 		int histSize = 256;
-		cv::calcHist(&img, 1, 0, Mat(), hist, 1, &histSize, 0);
+		cv::calcHist(&img, 1, 0, Mat(), hist, 1, &histSize, 0);//calculate histogram
 
 		Mat histImage = Mat::ones(200, 320, CV_8U) * 255;
 
-		normalize(hist, hist, 0, histImage.rows, CV_MINMAX, CV_32F);
+		normalize(hist, hist, 0, histImage.rows, CV_MINMAX, CV_32F);//normalize histogram
 
 		histImage = Scalar::all(255);
-		int binW = cvRound((double)histImage.cols / histSize);
+		int binW = cvRound((double)histImage.cols / histSize);//bin width
 
-		for (int i = 0; i < histSize; i++)
+		for (int i = 0; i < histSize; i++)//draw histogram chart
 			rectangle(histImage, Point(i * binW, histImage.rows),
 				Point((i + 1) * binW, histImage.rows - cvRound(hist.at<float>(i))),
 				Scalar::all(0), -1, 8, 0);
@@ -39,14 +38,15 @@ namespace cvex
 		waitKey();
 	}
 
+	
 	void rotatedRectangle(Mat& img, const RotatedRect& rec, const Scalar& color)
 	{
 		if (&rec == nullptr) return;
-		cv::Point2f rect_points[4];
+		cv::Point2f rect_points[4];//4 points of the rectangle
 		rec.points(rect_points);
 		for (int j = 0; j < 4; j++)
 		{
-			line(img, rect_points[j], rect_points[(j + 1) % 4], color);
+			line(img, rect_points[j], rect_points[(j + 1) % 4], color);//draw 4 edges of the rectangle
 		}
 	}
 
