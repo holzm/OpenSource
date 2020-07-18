a4fd1d53d888b0a218419a787cf24138e29a8226
holzm <54302524+holzm@users.noreply.github.com>
2020-07-19 02:53:52 +0800

    Update clahe.py

--- clahe.py
+++ clahe.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 
 clahe = cv2.createCLAHE(clipLimit=2,tileGridSize=(10,10))
 cl1 = clahe.apply(img)
-
-plt.subplot(131),plt.imshow(img,'gray')
 plt.subplot(132),plt.imshow(res,'gray')
 plt.subplot(133),plt.imshow(cl1,'gray')
 
